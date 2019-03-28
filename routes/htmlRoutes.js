eb092e29fc4ab9b169cf15ed5cafba0fca692ce0
Andrew Mayo <amayo0925@gmail.com>
2019-03-28 18:09:28 -0400

    finished

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+// ! var db = require("../models"); Do i need to put mongo db in models folder???
+
+// var path = require("path");
+
+// module.exports = function (app) {
+
+// app.get("/articles", function (req, res) {
+//     res.render("articles");
+// });
+
+// }
